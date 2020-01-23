d1ab134f4ae614cf08cca7c6915663bdb7dba1ec
Marcio Sarabando <marciosarabando@MacBook-Pro-de-Marcio.local>
2020-01-23 20:42:26 -0300

    Finalizacao do Backend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -7,10 +7,12 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
 //Todas as requisições que chegaram no servidor passará pelo BodyParser
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function(){
     console.log(`BACKEND FUNCIONANDO BEM NA PORTA ${port}.`)
