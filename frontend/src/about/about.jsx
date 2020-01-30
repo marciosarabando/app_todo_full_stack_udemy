e06cdbacf65624eccf4b96c606f5704d097bd6bc
Marcio Sarabando <marciosarabando@MacBook-Pro-de-Marcio.local>
2020-01-29 21:21:14 -0300

    Finalizacao do App Todo sem Redux

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageheader'
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="Sobre nós"></PageHeader>
+        <h2>Nossa história</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil voluptates doloremque quos molestias illum, quis impedit sapiente neque, placeat nulla optio voluptate iusto totam quam a odit unde quo quod.</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam illo repudiandae veniam consectetur ullam enim ab laboriosam odio asperiores ipsum rem dolore, quia magnam aut libero iure aliquam deserunt! Mollitia?</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro voluptas, at animi quos, nesciunt ratione corrupti ducimus veniam dolor soluta obcaecati eos repellendus explicabo atque facere tempora odio sunt cumque!</p>
+    </div>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,9 +3,13 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 import '../template/custom.css'
 
 import React from 'react'
+import Menu from '../template/menu'
+import Routes from './routes'
 
 export default props => (
     <div className='container'>
-      Teste
+      <Menu />
+      <Routes />
+    
     </div>
 )
